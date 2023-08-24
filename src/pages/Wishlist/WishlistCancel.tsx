7bda1b3962420093ddac6081e9bb8d8488495ebf
Trần Văn Sơn <42969529+synasapmob@users.noreply.github.com>
2023-08-24 14:10:41 +0700

    Feat/wishlist page (#2)
    * apply new page wishlist & add to wishlist, claim & cancel
    
    * (chore) fix logic show Button for Owner/Player
    
    * fix padding for button, change api when cancel wishlist

---
+++ src/pages/Wishlist/WishlistCancel.tsx
@@ -0,0 +1,34 @@
+import { Button } from '@chakra-ui/react';
+import { useAppSelector } from 'hooks/useRedux';
+import useSignAndSend from 'hooks/useSignAndSend';
+
+interface WishlistCancelProps {
+  trade_id: number;
+}
+
+export default ({ trade_id }: WishlistCancelProps) => {
+  const { account } = useAppSelector(state => state.injected.polkadot);
+  const { api } = useAppSelector(state => state.substrate);
+
+  const { mutation, isLoading } = useSignAndSend({
+    key: [`cancel_wishlist_detail/${trade_id}`],
+    address: account?.address as string,
+    onSuccess() {},
+  });
+
+  return (
+    <Button
+      variant="cancel"
+      padding={6}
+      borderRadius="3xl"
+      isLoading={isLoading}
+      onClick={() => {
+        if (api) {
+          mutation(api.tx.game.cancelTrade(trade_id, 'Wishlist'));
+        }
+      }}
+    >
+      Cancel wishlist
+    </Button>
+  );
+};
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,5 +1,6 @@
 export const GAFI_WALLET_STORAGE_KEY = 'GAFI_extension_connected';
 export const GAFI_WALLET_ACCOUNT_KEY = 'GAFI_WALLET_ACCOUNT';
+export const WISHLIST_STORAGE_KEY = 'WISHLIST_STORAGE_KEY';
 
 export const CHROME_EXT_URL =
   'https://chrome.google.com/webstore/detail/polkadot%7Bjs%7D-extension/mopnmbcafieddcagagdcbnhejhlodfdd';
